a5f941cb556abd2139f97a23f0d1b3a0248e65b6
Steed Huang <steedhuang2002@gmail.com>
2019-05-21 14:07:45 -0400

    added addRow function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-function addRow() {}
+function addRow() {
+  for (let i = 0; i < 4; i++) {
+    let tableRef = document
+      .getElementById("allmarks")
+      .getElementsByTagName("tbody")[0]
+      .getElementsByTagName("tbody")[i];
+
+    let newRow = tableRef.insertRow(tableRef.rows.length);
+
+    newInput0 = document.createElement("input");
+    newInput1 = document.createElement("input");
+    newInput0.setAttribute("id", "data");
+    newInput1.setAttribute("id", "data");
+
+    let newCell0 = newRow.insertCell(0);
+    let newCell1 = newRow.insertCell(1);
+    newCell0.appendChild(newInput0);
+    newCell1.appendChild(newInput1);
+  }
+}
 
 function getData() {}
 
