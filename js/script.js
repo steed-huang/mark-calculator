b4df582829560fc3e4cae29eb3ed6534583a4e75
Steed Huang <steedhuang2002@gmail.com>
2019-05-22 14:30:13 -0400

    added average calculation

--- js/script.js
+++ js/script.js
@@ -19,9 +19,74 @@ function addRow() {
   }
 }
 
-function getData() {}
+function getResult(score) {
+  alert(
+    "K/U: " +
+      score[0][0].toFixed(4) +
+      "%\n" +
+      "Think: " +
+      score[0][1].toFixed(4) +
+      "%\n" +
+      "Comm: " +
+      score[0][2].toFixed(4) +
+      "%\n" +
+      "App: " +
+      score[0][3].toFixed(4) +
+      "%\n\n" +
+      "Total Average: " +
+      score[1].toFixed(4) +
+      "%\n"
+  );
+}
 
-function getAverage() {}
+function getData() {
+  let marks = [[[], [], 25], [[], [], 25], [[], [], 25], [[], [], 25]];
+  for (let i = 0; i < 4; i++) {
+    let data = document
+      .getElementById("allmarks")
+      .getElementsByTagName("tbody")[0]
+      .getElementsByTagName("tbody")
+      [i].getElementsByTagName("input");
+
+    for (let z = 0; z < data.length; z++) {
+      if (z % 2 == 0) {
+        marks[i][0].push(Number(data[z].value));
+      } else {
+        marks[i][1].push(Number(data[z].value));
+      }
+    }
+  }
+  for (let i = 0; i < 4; i++) {
+    let secWeight = document
+      .getElementById("allmarks")
+      .getElementsByTagName("thead")[0]
+      .getElementsByTagName("tr")[1]
+      .getElementsByTagName("th")
+      [i].getElementsByTagName("input")[0];
+    marks[i][2] = Number(secWeight.value);
+  }
+  getResult(getAverage(marks));
+}
+
+function getAverage(marks) {
+  let secProd = [0, 0, 0, 0];
+  let weightSum = [0, 0, 0, 0];
+  let secAv = [0, 0, 0, 0];
+  let avAcc = 0;
+  for (let i = 0; i < 4; i++) {
+    for (let z = 0; z < marks[0][0].length; z++) {
+      secProd[i] += marks[i][0][z] * marks[i][1][z];
+      weightSum[i] += marks[i][1][z];
+    }
+  }
+  for (let i = 0; i < 4; i++) {
+    secAv[i] = secProd[i] / weightSum[i];
+  }
+  for (let i = 0; i < 4; i++) {
+    avAcc += secAv[i] * marks[i][2];
+  }
+  return [secAv, avAcc / 100];
+}
 
 function saveData() {}
 
